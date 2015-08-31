5b63a4710f475fe9225ac1a517079d4b54094e78
Carlos Ortega <carlos@goodrx.com>
2015-08-31 12:32:12 -0700

    adjust fs copypasta

--- generators/app/index.js
+++ generators/app/index.js
@@ -31,6 +31,10 @@ module.exports = yeoman.generators.Base.extend({
 
   writing: {
     app: function () {
+      this.mkdir('styles');
+      this.mkdir('scripts');
+      this.mkdir('images');
+
       this.fs.copy(
         this.templatePath('_package.json'),
         this.destinationPath('package.json')
@@ -39,6 +43,28 @@ module.exports = yeoman.generators.Base.extend({
         this.templatePath('_bower.json'),
         this.destinationPath('bower.json')
       );
+      this.fs.copy(
+        this.templatePath('_gulpfile.js');
+        this.destinationPath('gulpfile.js');
+      ):
+      this.fs.copy(
+        this.templatePath('_gitignore'),
+        this.destinationPath('.gitignore');
+      );
+      this.fs.copy(
+        this.templatePath('_index.jade'),
+        this.destinationPath('index.jade');
+      );
+      this.fs.copy(
+        this.templatePath('scripts/_scripts.js'),
+        this.destinationPath('scripts/scripts.js');
+      );
+
+      // this.fs.copyTpl('_script.coffee', 'src/scripts/scripts.coffee');
+      // this.fs.copyTpl('_script.js', 'src/scripts/scripts.js');
+      // this.fs.copyTpl('_style.styl', 'src/styles/styles.styl');
+      // this.fs.copyTpl('_style.scss', 'src/styles/styles.scss');
+      // this.template('_index.jade', 'src/index.jade');
     },
 
     projectfiles: function () {
